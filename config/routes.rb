3aa9d2807de9ec05e5f1a4b31a3a4e79e698e69e
deanfoulds <deanfoulds@gmail.com>
2018-08-22 09:21:02 +0100

    Phone number model added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :phone_numbers, only: [:new, :create]
+  post 'phone_numbers/verify' => 'phone_numbers#verify'
 end
